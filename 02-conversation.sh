1b0e6fe82f26387df69c2ef024cf30a34f9af758
palalle <palalle1024@gmail.com>
2025-05-22 03:12:35 -0500

    hello world

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "Trump : hey musk"
+PERSON1=TRUMP 
+PERSON2=MUSK
+
+echo "Trump:: Hey Musk, How are you?"
+echo "Musk:: Hello Trump, i am doing great.what about you"
+echo "Trump::Yeah i am doing well. what's up"
+echo "Musk::Getting ready to go mars.are you comming with me?"
+echo "Yeah. i will. i am excited about mars"
+echo "come on lets's jump on to mars"
