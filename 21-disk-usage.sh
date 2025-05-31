359da1348496fff1932035f5116a949a92cb8411
palalle <palalle1024@gmail.com>
2025-05-31 04:46:03 -0500

    hello world

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -9,43 +9,12 @@ IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 while IFS= read line 
 do
    USAGE=$(echo $line | awk '{print $6F}' |cut -d "%" -f1)
-   PARTITION=$(echo $line | awk '{print $7F}'
+   PARTITION=$(echo $line | awk '{print $7F}')
    if [ $USAGE -ge $DISK_THRESHOLD ]
    then 
        MSG+="High Disk Usage on $PARTITION: $USAGE % <br>"
    fi
 done <<< $DISK_USAGE 
 
-sh mail.sh "DevOps Team" "High Disk Usage" "$IP" "$MSG" "palalle1024@gmail.com" "ALERT_High Disk Usage"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+sh mail.sh  "DevOps Team" "High Disk Usage" "$IP" "$MSG" "palalle1024@gmail.com" "ALERT_High Disk Usage"
 
