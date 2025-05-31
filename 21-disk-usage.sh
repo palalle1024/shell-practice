2100074eead1d426c206d30d1d243609e1d28284
palalle <palalle1024@gmail.com>
2025-05-31 05:21:53 -0500

    hello world

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -4,7 +4,7 @@
 DISK_USAGE=$(df -hT | grep -v Filesystem) 
 DISK_THRESHOLD=1 # in a project it will be 75
 MSG=""
-#IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 while IFS= read line 
 do
@@ -12,9 +12,9 @@ do
    PARTITION=$(echo $line | awk '{print $7F}')
    if [ $USAGE -ge $DISK_THRESHOLD ]
    then 
-       MSG="High Disk Usage on $PARTITION: $USAGE %"
-       echo "$MSG+"
+       MSG+="High Disk Usage on $PARTITION: $USAGE % <br>"
    fi
 done <<< $DISK_USAGE 
 
-#sh mail.sh  "DevOps Team" "High Disk Usage" "$IP" "$MSG" "palalle1024@gmail.com" "ALERT_High Disk Usage"
+sh mail.sh  "DevOps Team" "High Disk Usage" "$IP" "$MSG" "palalle1024@gmail.com" "ALERT_High Disk Usage"
+
